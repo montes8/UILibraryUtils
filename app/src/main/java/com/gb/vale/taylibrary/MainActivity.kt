650a6bb35963504fa43a53838a21b80daf666d13
tayler <emontesinos812@gmail.com>
2024-02-02 13:15:55 -0500

    create component rating bar

--- app/src/main/java/com/gb/vale/taylibrary/MainActivity.kt
+++ app/src/main/java/com/gb/vale/taylibrary/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.gb.vale.uitaylibrary.animation.fadeOneToZero
+import com.gb.vale.uitaylibrary.extra.UiTayRatingBar
 import com.gb.vale.uitaylibrary.label.UiTayOtpCode
 
 
@@ -14,10 +15,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var btn = findViewById<UiTayOtpCode>(R.id.btnTest)
-        btn.uiTayOtpChangeListener = {
-            Log.d("otptay",it.first.toString())
-            Log.d("otptay",it.second.toString())
+        var btn = findViewById<UiTayRatingBar>(R.id.btnTest)
+        btn.onRatingBarSelectedListener={
+            Log.d("ratinu",it.toString())
         }
 
     }
