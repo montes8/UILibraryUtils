97c8601dcc3255a72c48106904fe87fef06789d7
tayler <emontesinos812@gmail.com>
2024-02-02 19:26:50 -0500

    version 1.1

--- app/src/main/java/com/gb/vale/taylibrary/MainActivity.kt
+++ app/src/main/java/com/gb/vale/taylibrary/MainActivity.kt
@@ -4,12 +4,10 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
