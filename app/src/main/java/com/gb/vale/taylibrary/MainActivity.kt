a8e2f2cf14ba551bc8667491f9be585289feac53
tayler <emontesinos812@gmail.com>
2024-03-09 21:58:21 -0500

    add component textview html ender link and image

--- app/src/main/java/com/gb/vale/taylibrary/MainActivity.kt
+++ app/src/main/java/com/gb/vale/taylibrary/MainActivity.kt
@@ -4,14 +4,22 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.gb.vale.uitaylibrary.label.UiTayTextHtml
 import com.gb.vale.uitaylibrary.utils.uiTayIsDeviceLocked
+import com.gb.vale.uitaylibrary.utils.uiTayShowToast
 
 class MainActivity : AppCompatActivity() {
 
+    val url = "<span style=\"font-weight:700\">Pyrin (PYI) </span><img src=\"https://www.unmineable.com/img/logos/PYI.png?v6\" style=\"height: 17px;width:17px;vertical-align: middle;\" alt=\"PYI icon\"> is now <a href=\"https://unmineable.com/coins/PYI\">available for mining</a>! start earning PYI not just with the <span style=\"font-weight:700\">PyrinHash</span> algorithm but with any of our <span style=\"font-weight:700\">CPU, Graphics Card or ASIC</span> compatible algorithms. Happy mining!"
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val text = findViewById<UiTayTextHtml>(R.id.tayTextHtml)
+        text.uiTayTextHtml = url
+        text.setOnTayTextHlmClickLink{
+            uiTayShowToast(it)
+        }
        Log.d("uiTayIsDeviceLocked",uiTayIsDeviceLocked(this).toString())
 
 
---
+++ uitaylibrary/src/main/java/com/gb/vale/uitaylibrary/label/imageurl/UiTayUrlLinkClick.kt
@@ -0,0 +1,71 @@
+package com.gb.vale.uitaylibrary.label.imageurl
+
+import android.text.Layout
+import android.text.Selection
+import android.text.Spannable
+import android.text.method.LinkMovementMethod
+import android.text.style.URLSpan
+import android.view.MotionEvent
+import android.widget.TextView
+import android.widget.Toast
+
+typealias UIClickLinkUrl = (String) -> Unit
+class UiTayUrlLinkClick : LinkMovementMethod() {
+
+     var uiClickLinkUrl: UIClickLinkUrl = {}
+    override fun onTouchEvent(
+        widget: TextView,
+        buffer: Spannable, event: MotionEvent
+    ): Boolean {
+        val action = event.action
+        if (action == MotionEvent.ACTION_UP || action == MotionEvent.ACTION_DOWN) {
+            var x = event.x.toInt()
+            var y = event.y.toInt()
+            x -= widget.totalPaddingLeft
+            y -= widget.totalPaddingTop
+            x += widget.scrollX
+            y += widget.scrollY
+            val layout: Layout = widget.layout
+            val line: Int = layout.getLineForVertical(y)
+            val off: Int = layout.getOffsetForHorizontal(line, x.toFloat())
+            val link = buffer.getSpans(off, off, URLSpan::class.java)
+            if (link.isNotEmpty()) {
+                if (action == MotionEvent.ACTION_UP) {
+                    var url = link[0].url.trim { it <= ' ' }
+                    if (url.startsWith("www")) {
+                        url = "http://$url"
+                    }
+                    if (url.startsWith("https://") || url.startsWith("http://") || url.startsWith("tel:") || url.startsWith(
+                            "mailto:"
+                        )
+                    ) {
+                        try {
+                            uiClickLinkUrl.invoke(url)
+                        } catch (e: Exception) {
+                            Toast.makeText(widget.context, "url no encontrada", Toast.LENGTH_LONG)
+                                .show()
+                        }
+                    }
+                } else {
+                    Selection.setSelection(
+                        buffer,
+                        buffer.getSpanStart(link[0]),
+                        buffer.getSpanEnd(link[0])
+                    )
+                }
+                return true
+            }
+        }
+        return super.onTouchEvent(widget, buffer, event)
+    }
+
+
+    companion object {
+        private var sInstance: UiTayUrlLinkClick? = null
+        val instance: UiTayUrlLinkClick?
+            get() {
+                if (sInstance == null) sInstance = UiTayUrlLinkClick()
+                return sInstance
+            }
+    }
+}
