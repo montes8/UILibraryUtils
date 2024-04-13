480c201a7b56fc3f65b121c590494c4c41c86a3b
tayler <emontesinos812@gmail.com>
2024-04-13 07:08:57 -0500

    update version

--- app/src/main/java/com/gb/vale/taylibrary/MainActivity.kt
+++ app/src/main/java/com/gb/vale/taylibrary/MainActivity.kt
@@ -2,11 +2,7 @@ package com.gb.vale.taylibrary
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
-import com.gb.vale.uitaylibrary.label.UiTayEditBasic
-import com.gb.vale.uitaylibrary.list.model.UiTayModelCustom
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,20 +13,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val ctn = findViewById<ConstraintLayout>(R.id.crnaa)
-        val text = findViewById<UiTayEditBasic>(R.id.listtest)
-        text.setListOptionCustomDropDawn(
-            arrayListOf(
-                UiTayModelCustom(title ="dfghjkl", subTitle = "dfghjk", message = "fvgbhjklkjhg"),
-                UiTayModelCustom(title ="dfghjkl", subTitle = "dfghjk", message = "fvgbhjklkjhg"),
-                UiTayModelCustom(title ="dfghjkl", subTitle = "dfghjk", message = "fvgbhjklkjhg")) )
-
-        text.setOnListClickTayEditListener(ctn,text){
-
         }
 
     }
 
 
 
-}
