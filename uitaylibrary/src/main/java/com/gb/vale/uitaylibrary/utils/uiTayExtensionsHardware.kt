18f5fe67161efe36f6ad6ad80653a1faf3d3c927
tayler <emontesinos812@gmail.com>
2024-07-13 08:28:24 -0500

    update fun

--- uitaylibrary/src/main/java/com/gb/vale/uitaylibrary/utils/uiTayExtensionsHardware.kt
+++ uitaylibrary/src/main/java/com/gb/vale/uitaylibrary/utils/uiTayExtensionsHardware.kt
@@ -3,26 +3,26 @@ package com.gb.vale.uitaylibrary.utils
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.telecom.Call
 import android.util.Log
 
-
-fun Context.uiTayViewDialedNumber(number : String = UI_TAY_EMPTY,key : String = "tel:",uiTayAction : String =
-    Intent.ACTION_DIAL
-){
-    val uri = Uri.parse("$key$number")
-    val intent = Intent(uiTayAction, uri)
-    try {
-        this.startActivity(intent)
-    } catch (e: SecurityException) {
-        Log.e(UI_TAY_TAG_ERROR,e.message.toString())
-    }
-}
 
 fun Context.uiTayDialedNumber(number : String = UI_TAY_EMPTY,key : String = "tel:",
     uiTayAction : String = Intent.ACTION_DIAL){
     try {
         val intent = Intent(uiTayAction)
         intent.setData(Uri.parse("$key$number"))
+        this.startActivity(intent)
+    } catch (e: SecurityException) {
+        Log.e(UI_TAY_TAG_ERROR,e.message.toString())
+    }
+}
+
+fun Context.uiTayViewCall(){
+    try {
+        val intent = Intent()
+        intent.setClass(this, Call::class.java)
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         this.startActivity(intent)
     } catch (e: SecurityException) {
         Log.e(UI_TAY_TAG_ERROR,e.message.toString())
